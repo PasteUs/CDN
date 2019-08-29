bf8eb555ab3fef3b4d4d27897db0bfec5e5bed38
Lucien Shui <lucien@lucien.ink>
2019-08-29 23:22:47 +0800

    Add version control

---
+++ version.sh
@@ -0,0 +1,22 @@
+#!/usr/local/bin sh
+
+FILE=version
+str=`cat ${FILE}`
+
+if [[ $# == 1 ]]; then
+    array=(${str//./ })
+    index=-1
+    if [[ $1 == "major" ]]; then
+		index=0
+	elif [[ $1 == "minor" ]]; then
+        index=1
+    elif [[ $1 == "patch" ]]; then
+        index=2
+    else
+        echo "Usage: ${0} [major|minor|patch]"
+    fi
+    (( array[${index}]++ ))
+    echo "${array[0]}.${array[1]}.${array[2]}" > ${FILE}
+else
+	echo ${str}
+fi
