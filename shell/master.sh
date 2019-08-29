e83c1c21cb6deb6a86c0ff8c1cef1a7fa9565c08
Lucien Shui <lucien@lucien.ink>
2019-08-30 00:25:29 +0800

    Update travis.yml

--- shell/master.sh
+++ shell/master.sh
@@ -4,7 +4,7 @@ GH_REF="github.com/PasteUs/CDN.git"
 
 bash -x shell/base.sh
 bash shell/version.sh patch
-git add version.txt
+git add --all
 git commit -m "pushed from travis-ci `TZ=UTC-8 date +'%Y-%m-%d %H:%M:%S'` [ci skip]"
 git push https://${GH_TOKEN}@${GH_REF} master
 VERSION=`cat version.txt`
