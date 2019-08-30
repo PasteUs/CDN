df56778c64681a5cfaee59e94f87ade47c79a4ff
Lucien Shui <lucien@lucien.ink>
2019-08-30 09:27:05 +0800

    Fix detached HEAD

--- shell/base.sh
+++ shell/base.sh
@@ -1,2 +1,5 @@
+rm -rf .git
+git clone https://github.com/PasteUs/CDN.git push_dir
+cd push_dir
 git config user.name "Lucien Shui"
 git config user.email "lucien@lucien.ink"
--- shell/master.sh
+++ shell/master.sh
@@ -3,10 +3,12 @@
 GH_REF="github.com/PasteUs/CDN.git"
 
 bash -x shell/base.sh
+cd push_dir
+
 bash shell/version.sh patch
-git add --all
+git add version.txt
 git commit -m "pushed from travis-ci `TZ=UTC-8 date +'%Y-%m-%d %H:%M:%S'` [ci skip]"
-git push https://${GH_TOKEN}@${GH_REF} master
 VERSION=`cat version.txt`
+echo ${VERSION}
 git tag ${VERSION} master
-git push https://${GH_TOKEN}@${GH_REF} ${VERSION}
+git push https://${GH_TOKEN}@${GH_REF} master ${VERSION}
