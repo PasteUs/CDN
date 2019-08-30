b79d6dd3f2d3b9a93ac4698b0f75562d518cff8d
Lucien Shui <lucien@lucien.ink>
2019-08-30 21:19:58 +0800

    ++version -> version++

--- shell/master.sh
+++ shell/master.sh
@@ -5,10 +5,10 @@ GH_REF="github.com/PasteUs/CDN.git"
 bash -x shell/base.sh
 cd push_dir
 
-bash shell/version.sh patch
-git add version.txt
-git commit -m "pushed from travis-ci `TZ=UTC-8 date +'%Y-%m-%d %H:%M:%S'` [ci skip]"
 VERSION=`cat version.txt`
 echo ${VERSION}
 git tag ${VERSION} master
+bash shell/version.sh patch
+git add version.txt
+git commit -m "pushed from travis-ci `TZ=UTC-8 date +'%Y-%m-%d %H:%M:%S'` [ci skip]"
 git push https://${GH_TOKEN}@${GH_REF} master ${VERSION}
