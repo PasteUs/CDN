9a1a0015776a31c9483135c197d943c6979239e5
Lucien <lucien@lucien.ink>
2019-09-02 17:53:58 +0800

    Add tag latest

--- shell/master.sh
+++ shell/master.sh
@@ -8,7 +8,8 @@ cd push_dir
 VERSION=`cat version.txt`
 echo ${VERSION}
 git tag ${VERSION} master
+git tag latest master
 bash shell/version.sh patch
 git add version.txt
 git commit -m "pushed from travis-ci `TZ=UTC-8 date +'%Y-%m-%d %H:%M:%S'` [ci skip]"
-git push https://${GH_TOKEN}@${GH_REF} master ${VERSION}
+git push https://${GH_TOKEN}@${GH_REF} master ${VERSION} latest
